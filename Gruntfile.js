67b8ba7825a09d9b21303d0f9313fe5baa7bdbd4
xuebaoxiang <1710342458@qq.com>
2020-03-18 18:36:56 +0800

    zhuyao

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+
+module.exports = function (grunt) {
+    grunt.initConfig({
+        htmlmin:{
+            options:{
+                removeComments:true,
+                collapseWhitespace:true
+            },
+            files: {
+                src:'./index.html',
+                dest:'dist/index.html'
+            }
+        },
+        cssmin: {
+            'dist/register.css':'register.css'
+        },
+        uglify: {
+            files: {'dist/register.js':'register.js'}
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('default', ['htmlmin','cssmin','uglify']);
+}
